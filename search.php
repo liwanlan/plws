fa04d4953dca7abfc425629765f7e4a9b27f017b
javebean <448267696@qq.com>
2016-03-11 09:12:45 +0800

    new
    Signed-off-by: javebean <448267696@qq.com>

---
+++ search.php
@@ -0,0 +1,269 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <meta name="format-detection" content="telephone=no"/>
+    <link rel="icon" href="images/favicon.ico" type="image/x-icon">
+    <title>Search</title>
+
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.css" rel="stylesheet">
+
+    <!-- Links -->
+
+    <link rel="stylesheet" href="css/search.css">
+
+
+    <!--JS-->
+    <script src="js/jquery.js"></script>
+    <script src="js/jquery-migrate-1.2.1.min.js"></script>
+    <script src="js/rd-smoothscroll.min.js"></script> 
+
+    <!--[if lt IE 9]>
+    <div style=' clear: both; text-align:center; position: relative;'>
+        <a href="http://windows.microsoft.com/en-US/internet-explorer/..">
+            <img src="images/ie8-panel/warning_bar_0000_us.jpg" border="0" height="42" width="820"
+                 alt="You are using an outdated browser. For a faster, safer browsing experience, upgrade for free today."/>
+        </a>
+    </div>
+    <script src="js/html5shiv.js"></script>
+    <![endif]-->
+    <script src='js/device.min.js'></script>
+  </head>
+  <body>
+  <div class="page">
+  <!--========================================================
+                            HEADER
+  =========================================================-->
+    
+<header class="head1">
+        <div id="stuck_container" class="stuck_container">
+            <nav class="navbar navbar-default navbar-static-top ">
+                <div class="container">
+                    <div class="navbar-header">
+                        <h1 class="navbar-brand">
+                            <a data-type='rd-navbar-brand' href="./">Constr<span>ex</span></a>
+                        </h1>
+                    </div>
+
+                    <ul class="navbar-nav sf-menu navbar-right" data-type="navbar">
+                        <li>
+                            <a href="./">Home</a>
+                        </li>
+                        <li class="dropdown">
+                            <a href="index-1.html">About</a>
+                            <ul class="dropdown-menu">
+                                <li>
+                                    <a href="#">Aliuam congu </a>
+                                </li>
+                                <li>
+                                    <a href="#">Ferentum nisl </a>
+                                </li>
+                                <li>
+                                    <a href="#">Mauris accan </a>
+                                    <ul class="dropdown-menu">
+                                        <li>
+                                            <a href="#">Aliuam congu</a>
+                                        </li>
+                                        <li>
+                                            <a href="#">Ferentum nisl</a>
+                                        </li>
+                                        <li>
+                                            <a href="#">Mauris accan </a>
+                                        </li>
+                                        <li>
+                                            <a href="#">Nullavel diam</a>
+                                        </li>
+                                    </ul>
+                                </li>
+                                <li>
+                                    <a href="#">Nullavel diam</a>
+                                </li>
+
+                            </ul>
+                        </li>
+                        <li>
+                            <a href="index-2.html">Services</a>
+                        </li>
+                        <li>
+                            <a href="index-3.html">Projects</a>
+                        </li>
+
+                        <li>
+                            <a href="index-4.html">Contacts</a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+
+            <section>
+                <div class="container">
+                    <div class="sform">
+                        <a class="search-form_toggle" href="#"></a>
+
+                        <form class="search-form" action="search.php" method="GET" accept-charset="utf-8">
+                            <label class="search-form_label">
+                                <input class="search-form_input" type="text" name="s" autocomplete="off"
+                                       placeholder="Search"/>
+                                <span class="search-form_liveout"></span>
+                            </label>
+                            <button class="search-form_submit fa-search" type="submit"></button>
+                        </form>
+                    </div>
+                </div>
+            </section>
+
+        </div>
+
+    </header>
+
+  <!--========================================================
+                            CONTENT
+  =========================================================-->
+  <main>
+    <section id="content" class="content well">
+        <div class="container">
+            <h3>Search Results</h3>
+            <div id="search-results"></div>
+        </div>
+    </section>
+  </main>  
+
+  <!--========================================================
+                            FOOTER
+  =========================================================-->
+  <footer>
+    
+    <section class="well_foot-1 bg1">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md-3 col-sm-6 col-xs-12">
+
+                            <h4>about us</h4>
+
+                            <p>
+                                Sed eleifend ornare metus, non rhoncus elit ultrices quis. Curabitur et metus ac sapien
+                                dictum tempus. Nam blandit sed ligula faucibus element um. Sed sed consequat nisi. Etiam
+                                mauris massa
+                                <br/>
+                                <br/>
+                                Sodales lobort dictum sollicitudin felis. Vestibulum tristique urna
+                            </p>
+                        </div>
+                        <div class="col-md-3 col-sm-6 col-xs-12">
+
+                            <h4>Flickr Widget</h4>
+
+                            <div class="img_flt">
+                                <img src="images/page-1_img8.jpg" alt=""/>
+                                <img src="images/page-1_img9.jpg" alt=""/>
+                                <img src="images/page-1_img10.jpg" alt=""/>
+                                <img src="images/page-1_img11.jpg" alt=""/>
+                                <img src="images/page-1_img12.jpg" alt=""/>
+                                <img src="images/page-1_img13.jpg" alt=""/>
+                            </div>
+                        </div>
+
+                        <div class="col-md-3 col-sm-6 col-xs-12 clear-md">
+
+                            <h4>Categories</h4>
+
+                            <div class="categ">
+                                <div>
+                                    <a href="#"> Sed eleifend </a>
+                                </div>
+                                <div>
+                                    <a href="#">Penareme</a>
+                                </div>
+                                <div>
+                                    <a href="#">Metus no</a>
+                                </div>
+                                <div>
+                                    <a href="#">tusnon </a>
+                                </div>
+                                <div>
+                                    <a href="#">rhoncus elit </a>
+                                </div>
+                                <div>
+                                    <a href="#">Curabitu</a>
+                                </div>
+                                <div>
+                                    <a href="#">elit ultrices</a>
+                                </div>
+                                <div>
+                                    <a href="#">ultrices quis</a>
+                                </div>
+                                <div>
+                                    <a href="#">Curabitur et us</a>
+                                </div>
+                            </div>
+
+                        </div>
+
+                        <div class="col-md-3 col-sm-6 col-xs-12">
+                            <h4>
+                                Follow us
+                            </h4>
+                            <ul class="link-list">
+                                <li>
+                                    <a href="#">
+                                        <span class="icon fa-google-plus"></span>
+                                        Google +
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="#">
+                                        <span class="icon fa-twitter"></span>
+                                        Twitter
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="#">
+                                        <span class="icon fa-facebook"></span>
+                                        Facebook
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="#">
+                                        <span class="icon fa-linkedin"></span>
+                                        LinkedIn
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="#">
+                                        <span class="icon fa-rss"></span>
+                                        RSS
+                                    </a>
+                                </li>
+                            </ul>
+                        </div>
+
+                    </div>
+                </div>
+            </section>
+
+
+            <section class="well_foot-2">
+                <div class="container">
+                    <div class="rights">
+                        <h1>Constr<span>ex</span></h1> &#169; <span id="copyright-year"></span>&nbsp;
+                        <a href="index-5.html">Privacy Policy</a>
+                    </div>
+                </div>
+            </section>
+    
+  </footer>
+  </div>
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <!-- Include all compiled plugins (below), or include individual files as needed -->         
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/tm-scripts.js"></script>    
+  <!-- </script> -->
+
+
+  </body>
+</html>
